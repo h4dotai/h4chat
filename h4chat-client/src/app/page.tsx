0441e04846b65b0c7f112f2c805185abf27eb51a
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-05-16 16:43:39 +0530

    fixing the entry point of chat app

--- h4chat-client/src/app/page.tsx
+++ h4chat-client/src/app/page.tsx
@@ -1,8 +1,25 @@
-import { redirect } from "next/navigation"
+"use client"
+
+import { useRouter } from "next/navigation"
 import { v4 as uuidv4 } from "uuid"
 
 export default function Home() {
-  const roomId = uuidv4()
+  const router = useRouter()
 
-  redirect(`/room/${roomId}`)
+  const createRoom = () => {
+    const roomId = uuidv4()
+
+    router.push(`/room/${roomId}`)
+  }
+
+  return (
+    <main className="h-screen bg-black text-zinc-400 flex items-center justify-center font-mono">
+      <button
+        onClick={createRoom}
+        className="border border-zinc-800 px-6 py-3 hover:bg-zinc-900 transition"
+      >
+        create secure room
+      </button>
+    </main>
+  )
 }
